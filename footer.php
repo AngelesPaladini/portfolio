850c95526b8092ad72566481a1b0f9649e5ae622
AngelesPaladini <paladiniangeles@gmail.com>
2021-10-19 16:22:53 -0300

    modificacion footer

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer class="footer mt-auto py-3 pb-5 text-center text-sm-left">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-3">
+          <a href="https://github.com/AngelesPaladini" target="_blank" title="Github"><i class="fab fa-github"></i></a>
+          <a href="https://www.linkedin.com/in/angeles-paladini-3bb8761ab/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+        <div class="col-12 col-sm-3">
+          Sponsor <a href="http://depcsuite.com">DePC Suite</a>
+        </div>
+        <div class="col-12 col-sm-3">
+          <a href="mailto:paladiniangeles@gmail.com">paladiniangeles@gmail.com</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <div class="whatsapp">
+    <a href="https://api.whatsapp.com/send?phone=541155609406" title="Whatsapp"><i class="fab fa-whatsapp"></i></a>
+  </div>
+  <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
+  
